6109c3ec210534fb0eb9f58019fb1d25b0e9b59d
Jonathan Tyler Weber <jonathantweber@gmail.com>
2023-11-11 15:36:46 +0800

    change result to str

--- app.py
+++ app.py
@@ -3,47 +3,47 @@ from operations import add, sub, mult, div
 
 app = Flask(__name__)
 
-# operations = {
-#     'add': add,
-#     'sub': sub,
-#     'mult': mult,
-#     'div': div
-# }
-
-# @app.route('/<operation>')
-# def do_math(operation):
+# @app.route('/add')
+# def add_numbers():
 #     a = int(request.args.get('a'))
 #     b = int(request.args.get('b'))
-#     result = operations[operation](a, b)
+#     result = add(a, b)
 #     return result
 
-@app.route('/add')
-def add_numbers():
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    result = operations['add'](a, b)
-    return result
+# @app.route('/sub')
+# def subtract_numbers():
+#     a = int(request.args.get('a'))
+#     b = int(request.args.get('b'))
+#     result = sub(a, b)
+#     return result
 
-@app.route('/sub')
-def subtract_numbers():
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    result = operations['sub'](a, b)
-    return result
+# @app.route('/mult')
+# def multiply_numbers():
+#     a = int(request.args.get('a'))
+#     b = int(request.args.get('b'))
+#     result = mult(a, b)
+#     return result
 
-@app.route('/mult')
-def multiply_numbers():
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    result = operations['mult'](a, b)
-    return result
+# @app.route('/div')
+# def divide_numbers():
+#     a = int(request.args.get('a'))
+#     b = int(request.args.get('b'))
+#     result = div(a, b)
+#     return result
 
-@app.route('/div')
-def divide_numbers():
+operations = {
+    'add': add,
+    'sub': sub,
+    'mult': mult,
+    'div': div
+}
+
+@app.route('/<operation>')
+def do_math(operation):
     a = int(request.args.get('a'))
     b = int(request.args.get('b'))
-    result = operations['div'](a, b)
-    return result
+    result = operations[operation](a, b)
+    return str(result)
     
 
 
